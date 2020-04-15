be171a38cc2b012ea38e3e72cacd943ba90cffb7
EC2 Default User <devonteduncan202@gmail.com>
2020-04-15 06:01:41 +0000

    moved bulma to public

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 8080
+const path = require('path')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname ,'public/views' , 'index.html'))
+})
 
 app.listen(port, () => {
 	console.log(`Now listening on port ${port}`)
