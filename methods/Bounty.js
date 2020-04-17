ebc26575372e2eb77b986e980edefc80e0f1c2b0
Enmanuel <devonteduncan202@gmail.com>
2020-04-17 02:13:27 +0000

    Fixed bounty variables, added env package

--- methods/Bounty.js
+++ methods/Bounty.js
@@ -1,23 +1,23 @@
 const db = require('../db');
 
 class Bounty {
-  static createBounty(userID,img, cost, info, category , location) {
+  static createBounty(user_id,img, cost, info, category , location) {
     const queryText = 'INSERT INTO bounties (owner, thumbnail, cost, info, category, location) VALUES ($1, $2, $3, $4, $5, $6);';
-    return db.query(queryText, [userID, img, cost, info, category, location]);
+    return db.query(queryText, [user_id, img, cost, info, category, location]);
   }
-  static updateBounty(id, cost, category, info, location){
+  static updateBounty(user_id, cost, category, info, location){
   	const queryText = 'UPDATE bounties SET cost = $2, category = $3, info = $4, location = $5 WHERE owner = $1;';
-  	return db.query([id, cost, category, info, location])
+  	return db.query([user_id, cost, category, info, location])
   		.then((data) => data.rows)
   }
-  static deleteBounty(bountyId, userId){
+  static deleteBounty(bountyId, user_id){
   	const queryText = 'DELETE FROM bounties WHERE id = $1 AND owner = $2;';
-  	return db.query(queryText, [bountyId, userId]);
+  	return db.query(queryText, [bountyId, user_id]);
   }
 
-  static getUsersBounties(id) {
+  static getUsersBounties(user_id) {
     const queryText = 'SELECT * FROM bounties WHERE owner = $1;';
-    return db.query(queryText, [id])
+    return db.query(queryText, [user_id])
     	.then((data) => data.rows);
   }
 
