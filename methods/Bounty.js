2e33e794b50deaeba28e1e295f48f00dba8b2ddf
Deteriator <markgriffith009@gmail.com>
2020-04-19 17:21:40 -0400

    changed update function to reflect proper bounty.

--- methods/Bounty.js
+++ methods/Bounty.js
@@ -1,28 +1,28 @@
 const db = require('../db');
 
 class Bounty {
-  static createBounty(userID,img, cost, info, category , location) {
-    const queryText = 'INSERT INTO bounties (owner, thumbnail, cost, info, category, location) VALUES ($1, $2, $3, $4, $5, $6);';
-    return db.query(queryText, [userID, img, cost, info, category, location]);
+  static createBounty(name, user_id, img, cost, info, category , location) {
+    const queryText = 'INSERT INTO bounties (owner, thumbnail, cost, info, category, location, name) VALUES ($1, $2, $3, $4, $5, $6, $7);';
+    return db.query(queryText, [user_id, img, cost, info, category, location, name]);
   }
-  static updateBounty(id, cost, category, info, location){
-  	const queryText = 'UPDATE bounties SET cost = $2, category = $3, info = $4, location = $5 WHERE owner = $1;';
-  	return db.query([id, cost, category, info, location])
+  static updateBounty(user_id, bountyId, cost, category, info, location){
+  	const queryText = 'UPDATE bounties SET cost = $3, category = $4, info = $5, location = $6 WHERE owner = $1 AND bounty_id = $2;';
+  	return db.query(queryText,[user_id,bountyId, cost, category, info, location])
   		.then((data) => data.rows)
   }
-  static deleteBounty(bountyId, userId){
-  	const queryText = 'DELETE FROM bounties WHERE id = $1 AND owner = $2;';
-  	return db.query(queryText, [bountyId, userId]);
+  static deleteBounty(bountyId, user_id){
+  	const queryText = 'DELETE FROM bounties WHERE bounty_id = $1 AND owner = $2;';
+  	return db.query(queryText, [bountyId, user_id]);
   }
 
-  static getUsersBounties(id) {
+  static getUsersBounties(user_id) {
     const queryText = 'SELECT * FROM bounties WHERE owner = $1;';
-    return db.query(queryText, [id])
+    return db.query(queryText, [user_id])
     	.then((data) => data.rows);
   }
 
   static getAllBounties() {
-    const queryText = 'SELECT * FROM bounites;';
+    const queryText = 'SELECT * FROM bounties;';
     return db.query(queryText)
     	.then((data) => data.rows);
   }
