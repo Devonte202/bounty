1d8a3c64131ac0264356401d0b3be67348e01e6d
Devonte G. Duncan <devonteduncan202@gmail.com>
2020-04-19 18:43:26 -0400

    Updated update

--- methods/Bounty.js
+++ methods/Bounty.js
@@ -5,9 +5,9 @@ class Bounty {
     const queryText = 'INSERT INTO bounties (owner, thumbnail, cost, info, category, location, name) VALUES ($1, $2, $3, $4, $5, $6, $7);';
     return db.query(queryText, [user_id, img, cost, info, category, location, name]);
   }
-  static updateBounty(user_id, bountyId, cost, category, info, location){
-  	const queryText = 'UPDATE bounties SET cost = $3, category = $4, info = $5, location = $6 WHERE owner = $1 AND bounty_id = $2;';
-  	return db.query(queryText,[user_id,bountyId, cost, category, info, location])
+  static updateBounty(user_id, bountyId, cost, category, info, name){
+  	const queryText = 'UPDATE bounties SET cost = $3, category = $4, info = $5, name = $6 WHERE owner = $1 AND bounty_id = $2;';
+  	return db.query(queryText,[user_id,bountyId, cost, category, info, name])
   		.then((data) => data.rows)
   }
   static deleteBounty(bountyId, user_id){
