dffaf3e4dc207bca778367f80c9d4979eeab73dc
Deteriator <markgriffith009@gmail.com>
2020-04-19 21:03:29 -0400

    ssl hotfix

--- db.js
+++ db.js
@@ -7,7 +7,6 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
 })
 
 module.exports = {
